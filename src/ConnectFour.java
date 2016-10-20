aad36f98650b87bb1ad4a3640ae16fd47cc2550d
miservilla <miservilla@gmail.com>
2016-10-20 04:48:24 -0600

    Initial commit

---
+++ src/ConnectFour.java
@@ -0,0 +1,241 @@
+import java.awt.Color;
+
+/**
+ * Class to play a game of Connect 4.
+ * Computer plays black, human plays white.
+ */
+public class ConnectFour {
+
+    /** Number of columns on the board. */
+    public static final int COLUMNS = 7;
+
+    /** Number of rows on the board. */
+    public static final int ROWS = 6;
+
+    /** Color for computer player's pieces */
+    public static final Color COMPUTER = Color.BLACK;
+
+    /** Color for human player's pieces */
+    public static final Color HUMAN = Color.RED;
+
+    /** Color for blank spaces. */
+    public static final Color NONE = Color.WHITE;
+
+
+    /**
+     * Drops a piece of given Color in column.  Modifies the board
+     * array. Assumes the move is legal.
+     *
+     * @param board The game board.
+     * @param color Color of piece to drop.
+     * @param column Column in which to drop the piece.
+     */
+    public static void dropPiece(Color[][] board, Color color, int column) {
+        // TODO You have to write this.
+    }
+
+    /**
+     * Checks if the board is full.
+     * @param board The game board.
+     * @return True if board is full, false if not.
+     */
+    public static boolean isFull(Color[][] board) {
+        // TODO You have to write this.
+        return false;
+    }
+
+    /**
+     * Checks if dropping a piece into the given column would be a
+     * legal move.
+     *
+     * @param board The game board.
+     * @param column The column to check.
+     * @return true if column is neither off the edge of the board nor full.
+     */
+    public static boolean isLegal(Color[][] board, int column) {
+        // TODO You have to write this.
+        return false;
+    }
+
+    /**
+     * Given the color of a player, return the color for the opponent.
+     * Returns human player color when given computer player color.
+     * Returns computer player color when given human player color.
+     * @param color Player color.
+     * @return Opponent color.
+     */
+    public static Color getOpposite(Color color) {
+        // TODO You have to write this.
+        return null;
+    }
+
+    /**
+     * Check for a win starting at a given location and heading in a
+     * given direction.
+     *
+     * Returns the Color of the player with four in a row starting at
+     * row r, column c and advancing rowOffset, colOffset each step.
+     * Returns NONE if no player has four in a row here, or if there
+     * aren't four spaces starting here.
+     *
+     * For example, if rowOffset is 1 and colOffset is 0, we would
+     * advance the row index by 1 each step, checking for a vertical
+     * win. Similarly, if rowOffset is 0 and colOffset is 1, we would
+     * check for a horizonal win.
+     * @param board The game board.
+     * @param r Row index of where win check starts
+     * @param c Column index of where win check starts
+     * @param rowOffset How much to move row index at each step
+     * @param colOffset How much to move column index at each step
+     * @return Color of winner from given location in given direction
+     *         or NONE if no winner there.
+     */
+    public static Color findLocalWinner(Color[][] board, int r, int c,
+                                        int rowOffset, int colOffset) {
+        // TODO You have to write this.
+        return null;
+    }
+
+    /**
+     * Checks entire board for a win (4 in a row).
+     *
+     * @param board The game board.
+     * @return color (HUMAN or COMPUTER) of the winner, or NONE if no
+     * winner yet.
+     */
+    public static Color findWinner(Color[][] board) {
+        // TODO You have to write this.
+        // HINT: You should call the findLocalWinner method to help you
+        return null;
+    }
+
+    /**
+     * Returns computer player's best move.
+     * @param board The game board.
+     * @param maxDepth Maximum search depth.
+     * @return Column index for computer player's best move.
+     */
+    public static int bestMoveForComputer(Color[][] board, int maxDepth) {
+        // TODO You have to write this.
+        // Hint: this will be similar to max
+        return -1;
+    }
+
+    /**
+     * Returns the value of board with computer to move:
+     *     1 if computer can force a win,
+     *     -1 if computer cannot avoid a loss,
+     *     0 otherwise.
+     *
+     * @param board The game board.
+     * @param maxDepth Maximum search depth.
+     * @param depth Current search depth.
+     */
+    public static int max(Color[][] board, int maxDepth, int depth) {
+        // TODO You have to write this.
+        // Hint: this will be similar to min
+        return 0;
+    }
+
+    /**
+     * Returns the value of board with human to move: 
+     *    1 if human cannot avoid a loss,
+     *    -1 if human can force a win,
+     *     0 otherwise.
+     *
+     * @param board The game board.
+     * @param maxDepth Maximum search depth.
+     * @param depth Current search depth.
+     */
+    public static int min(Color[][] board, int maxDepth, int depth) {
+
+        // The comments in this method are rather verbose to help you
+        // understand what is going on. I don't expect you to be so
+        // wordy in your own code.
+
+        // First, see if anyone is winning already
+        Color winner = findWinner(board);
+        if (winner == COMPUTER) {
+            // computer is winning, so human is stuck
+            return 1;
+        } else if (winner == HUMAN) {
+            // human already won, no chance for computer
+            return -1;
+        } else if (isFull(board) || (depth == maxDepth)) {
+            // We either have a tie (full board) or we've searched as
+            // far as we can go. Either way, call it a draw.
+            return 0;
+        } else {
+            // At this point, we know there isn't a winner already and
+            // that there must be at least one column still available
+            // for play. We'll search all possible moves for the human
+            // player and decide which one gives the lowest (best for
+            // human) score, assuming that the computer would play
+            // perfectly.
+
+            // Start off with a value for best result that is larger
+            // than any possible result.
+            int bestResult = 2;
+
+            // Loop over all columns to test them in turn.
+            for (int c = 0; c < COLUMNS; c++) {
+                if (isLegal(board, c)) {
+                    // This column is a legal move. We'll drop a piece
+                    // there so we can see how good it is.
+                    dropPiece(board, HUMAN, c);
+                    // Call max to see what the value would be for the
+                    // computer's best play. The max method will end
+                    // up calling min in a similar fashion in order to
+                    // figure out the best result for the computer's
+                    // turn, assuming the human will play perfectly in
+                    // response.
+                    int result = max(board, maxDepth, depth + 1);
+                    // Now that we have the result, undo the drop so
+                    // the board will be like it was before.
+                    undoDrop(board, c);
+
+                    if (result <= bestResult) {
+                        // We've found a new best score. Remember it.
+                        bestResult = result;
+                    }
+                }
+            }
+            return bestResult;
+        }
+    }
+
+
+    /**
+     * Removes the top piece from column. Modifies board. Assumes
+     * column is not empty.
+     * @param board The game board.
+     * @param column Column with piece to remove.
+     */
+    public static void undoDrop(Color[][] board, int column) {
+        // We'll start at the top and loop down the column until we
+        // find a row with a piece in it. 
+        int row = ROWS - 1;
+        while(board[row][column] == NONE && row > 0) {
+            row--;
+        }
+
+        // Set the top row that had a piece to empty again.
+        board[row][column] = NONE;
+    }
+
+    /** Creates board array and starts game GUI. */
+    public static void main(String[] args) {
+        // create array for game board
+        Color[][] board = new Color[ROWS][COLUMNS];
+        // fill board with empty spaces
+        for(int row = 0; row < ROWS; row++) {
+            for (int col = 0; col < COLUMNS; col++) {
+                board[row][col] = NONE;
+            }
+        }
+
+        // show the GUI and start the game
+        ConnectFourGUI.showGUI(board, HUMAN, 5);
+    }
+
+}
---
+++ src/ConnectFourTest.java
@@ -0,0 +1,277 @@
+/**
+ * Class to test the ConnectFour methods.
+ * If your code does not pass these tests, it is broken.
+ *
+ * It is possible that your code is broken even if it passes these
+ * tests, so make sure the game plays properly, too.
+ */
+public class ConnectFourTest {
+
+    private static int correctTests = 0;
+    private static int totalTests = 0;
+
+    public static void testConstants() {
+        // Make sure the constants in ConnectFour.java are as they
+        // should be.
+
+        // Board size should be as expected
+        countTest(7 == ConnectFour.COLUMNS);
+        countTest(6 == ConnectFour.ROWS);
+
+        // Student could change player colors, I guess, but shouldn't
+        // break the logic in the process.
+        countTest(ConnectFour.COMPUTER != ConnectFour.HUMAN);
+        countTest(ConnectFour.HUMAN != ConnectFour.NONE);
+        countTest(ConnectFour.NONE != ConnectFour.COMPUTER);
+    }
+
+    /** Utility method to generate empty board. */
+    public static java.awt.Color[][] emptyBoard() {
+        java.awt.Color[][] result =
+                new java.awt.Color[ConnectFour.ROWS][ConnectFour.COLUMNS];
+        for (int r = 0; r < result.length; r++) {
+            for (int c = 0; c < result[r].length; c++) {
+                result[r][c] = ConnectFour.NONE;
+            }
+        }
+        return result;
+    }
+
+    public static void testGetOpposite() {
+        countTest(ConnectFour.HUMAN == ConnectFour.getOpposite(ConnectFour.COMPUTER));
+        countTest(ConnectFour.COMPUTER == ConnectFour.getOpposite(ConnectFour.HUMAN));
+    }
+
+    public static void testDropPiece() {
+        java.awt.Color[][] board = emptyBoard();
+        ConnectFour.dropPiece(board, ConnectFour.COMPUTER, 2);
+        countTest(ConnectFour.COMPUTER == board[0][2]);
+        countTest(ConnectFour.NONE == board[1][2]);
+        countTest(ConnectFour.NONE == board[0][1]);
+        countTest(ConnectFour.NONE == board[0][3]);
+        ConnectFour.dropPiece(board, ConnectFour.HUMAN, 2);
+        countTest(ConnectFour.HUMAN == board[1][2]);
+        countTest(ConnectFour.COMPUTER == board[0][2]);
+        countTest(ConnectFour.NONE == board[3][2]);
+    }
+
+    public static void testIsLegal() {
+        java.awt.Color[][] board = emptyBoard();
+        countTest(!ConnectFour.isLegal(board, -1));
+        countTest(!ConnectFour.isLegal(board, 7));
+        countTest(ConnectFour.isLegal(board, 6));
+        ConnectFour.dropPiece(board, ConnectFour.COMPUTER, 2);
+        ConnectFour.dropPiece(board, ConnectFour.HUMAN, 2);
+        ConnectFour.dropPiece(board, ConnectFour.COMPUTER, 2);
+        ConnectFour.dropPiece(board, ConnectFour.HUMAN, 2);
+        ConnectFour.dropPiece(board, ConnectFour.COMPUTER, 2);
+        countTest(ConnectFour.isLegal(board, 2));
+        ConnectFour.dropPiece(board, ConnectFour.HUMAN, 2);
+        countTest(!ConnectFour.isLegal(board, 2));
+    }
+
+    public static void testIsFull() {
+        java.awt.Color[][] board = emptyBoard();
+        for (int r = 0; r < ConnectFour.ROWS; r++) {
+            for (int c = 0; c < ConnectFour.COLUMNS; c++) {
+                countTest(!ConnectFour.isFull(board));
+                ConnectFour.dropPiece(board, ConnectFour.COMPUTER, c);
+            }
+        }
+        countTest(ConnectFour.isFull(board));
+    }
+
+    public static void testHorizontalWinner() {
+        java.awt.Color[][] board = emptyBoard();
+        countTest(ConnectFour.NONE == ConnectFour.findWinner(board));
+        for (int c = 3; c < 7; c++) {
+            ConnectFour.dropPiece(board, ConnectFour.COMPUTER, c);
+        }
+        countTest(ConnectFour.COMPUTER == ConnectFour.findWinner(board));
+        countTest(ConnectFour.COMPUTER == ConnectFour.findLocalWinner(board, 0, 3, 0, 1));
+        countTest(ConnectFour.COMPUTER == ConnectFour.findLocalWinner(board, 0, 6, 0, -1));
+        countTest(ConnectFour.NONE == ConnectFour.findLocalWinner(board, 0, 2, 0, 1));
+        countTest(ConnectFour.NONE == ConnectFour.findLocalWinner(board, 0, 4, 0, 1));
+    }
+
+    public static void testVerticalWinner() {
+        java.awt.Color[][] board = emptyBoard();
+        for (int i = 0; i < 4; i++) {
+            ConnectFour.dropPiece(board, ConnectFour.HUMAN, 6);
+        }
+        countTest(ConnectFour.HUMAN == ConnectFour.findWinner(board));
+        countTest(ConnectFour.HUMAN == ConnectFour.findLocalWinner(board, 0, 6, 1, 0));
+        countTest(ConnectFour.NONE == ConnectFour.findLocalWinner(board, 1, 6, 1, 0));
+        countTest(ConnectFour.HUMAN == ConnectFour.findLocalWinner(board, 3, 6, -1, 0));
+        countTest(ConnectFour.NONE == ConnectFour.findLocalWinner(board, 4, 6, -1, 0));
+    }
+
+    public static void testVerticalWinner2() {
+        java.awt.Color[][] board = emptyBoard();
+        ConnectFour.dropPiece(board, ConnectFour.COMPUTER, 3);
+        ConnectFour.dropPiece(board, ConnectFour.COMPUTER, 3);
+        for (int i = 0; i < 4; i++) {
+            countTest(ConnectFour.NONE == ConnectFour.findWinner(board));
+            ConnectFour.dropPiece(board, ConnectFour.HUMAN, 3);
+        }
+        countTest(ConnectFour.HUMAN == ConnectFour.findWinner(board));
+        countTest(ConnectFour.HUMAN == ConnectFour.findLocalWinner(board, 2, 3, 1, 0));
+        countTest(ConnectFour.NONE == ConnectFour.findLocalWinner(board, 1, 3, 1, 0));
+        countTest(ConnectFour.HUMAN == ConnectFour.findLocalWinner(board, 5, 3, -1, 0));
+        countTest(ConnectFour.NONE == ConnectFour.findLocalWinner(board, 4, 3, -1, 0));
+    }
+
+
+    public static void testDiagonalWinner1() {
+        java.awt.Color[][] board = emptyBoard();
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < i; j++) {
+                ConnectFour.dropPiece(board, ConnectFour.HUMAN, i);
+            }
+            ConnectFour.dropPiece(board, ConnectFour.COMPUTER, i);
+        }
+        countTest(ConnectFour.COMPUTER == ConnectFour.findWinner(board));
+        countTest(ConnectFour.COMPUTER == ConnectFour.findLocalWinner(board, 0, 0, 1, 1));
+        countTest(ConnectFour.NONE == ConnectFour.findLocalWinner(board, 1, 1, 1, 1));
+        countTest(ConnectFour.COMPUTER == ConnectFour.findLocalWinner(board, 3, 3, -1, -1));
+        countTest(ConnectFour.NONE == ConnectFour.findLocalWinner(board, 4, 4, -1, -1));    }
+
+    public static void testDiagonalWinner2() {
+        java.awt.Color[][] board = emptyBoard();
+        for (int i = 0; i < 4; i++) {
+            for (int j = 0; j < i; j++) {
+                ConnectFour.dropPiece(board, ConnectFour.COMPUTER, 5 - i);
+            }
+            ConnectFour.dropPiece(board, ConnectFour.HUMAN, 5 - i);
+        }
+        countTest(ConnectFour.HUMAN == ConnectFour.findWinner(board));
+        countTest(ConnectFour.HUMAN == ConnectFour.findLocalWinner(board, 0, 5, 1, -1));
+        countTest(ConnectFour.NONE == ConnectFour.findLocalWinner(board, 1, 4, 1, -1));
+        countTest(ConnectFour.HUMAN == ConnectFour.findLocalWinner(board, 3, 2, -1, 1));
+        countTest(ConnectFour.NONE == ConnectFour.findLocalWinner(board, 4, 1, -1, 1));
+    }
+
+    public static void testUndoDrop() {
+        java.awt.Color[][] board = emptyBoard();
+        ConnectFour.dropPiece(board, ConnectFour.COMPUTER, 2);
+        ConnectFour.dropPiece(board, ConnectFour.HUMAN, 2);
+        ConnectFour.undoDrop(board, 2);
+        countTest(ConnectFour.COMPUTER == board[0][2]);
+        countTest(ConnectFour.NONE == board[1][2]);
+    }
+
+    public static void testMax0() {
+        java.awt.Color[][] board = emptyBoard();
+        for (int i = 0; i < 3; i++) {
+            ConnectFour.dropPiece(board, ConnectFour.COMPUTER, i);
+            ConnectFour.dropPiece(board, ConnectFour.HUMAN,
+                    ConnectFour.COLUMNS - 1 - i);
+        }
+        countTest(0 == ConnectFour.max(board, 0, 0));
+        ConnectFour.dropPiece(board, ConnectFour.COMPUTER, 3);
+        countTest(1 == ConnectFour.max(board, 0, 0));
+        ConnectFour.undoDrop(board, 3);
+        ConnectFour.dropPiece(board, ConnectFour.HUMAN, 3);
+        countTest(-1 == ConnectFour.max(board, 0, 0));
+    }
+
+    public static void testMax1() {
+        java.awt.Color[][] board = emptyBoard();
+        // Computer cannot win in one move
+        countTest(0 == ConnectFour.max(board, 1, 0));
+        ConnectFour.dropPiece(board, ConnectFour.COMPUTER, 0);
+        ConnectFour.dropPiece(board, ConnectFour.COMPUTER, 0);
+        ConnectFour.dropPiece(board, ConnectFour.COMPUTER, 0);
+        // Now computer can win in one move
+        countTest(1 == ConnectFour.max(board, 1, 0));
+    }
+
+    public static void testMax2() {
+        java.awt.Color[][] board = emptyBoard();
+        for (int i = 0; i < 3; i++) {
+            ConnectFour.dropPiece(board, ConnectFour.HUMAN, i + 2);
+        }
+        // Computer cannot prevent human from winning
+        countTest(-1 == ConnectFour.max(board, 2, 0));
+        ConnectFour.dropPiece(board, ConnectFour.COMPUTER, 0);
+        ConnectFour.dropPiece(board, ConnectFour.COMPUTER, 0);
+        ConnectFour.dropPiece(board, ConnectFour.COMPUTER, 0);
+        // Now computer can win in one move
+        countTest(1 == ConnectFour.max(board, 2, 0));
+    }
+
+    public static void testMin3() {
+        java.awt.Color[][] board = emptyBoard();
+        for (int i = 0; i < 2; i++) {
+            ConnectFour.dropPiece(board, ConnectFour.HUMAN, i + 2);
+        }
+        // Human can win in three moves
+        countTest(-1 == ConnectFour.min(board, 3, 0));
+    }
+
+    public static void testBestMoveForComputer() {
+        java.awt.Color[][] board = emptyBoard();
+        for (int i = 0; i < 3; i++) {
+            ConnectFour.dropPiece(board, ConnectFour.HUMAN, 5);
+        }
+        // Computer has to block
+        countTest(5 == ConnectFour.bestMoveForComputer(board, 3));
+    }
+
+    public static void testDeepSearch() {
+        java.awt.Color[][] board = emptyBoard();
+        ConnectFour.dropPiece(board, ConnectFour.HUMAN, 0);
+        ConnectFour.dropPiece(board, ConnectFour.COMPUTER, 1);
+        ConnectFour.dropPiece(board, ConnectFour.HUMAN, 2);
+        ConnectFour.dropPiece(board, ConnectFour.COMPUTER, 5);
+        ConnectFour.dropPiece(board, ConnectFour.COMPUTER, 6);
+        ConnectFour.dropPiece(board, ConnectFour.COMPUTER, 0);
+        ConnectFour.dropPiece(board, ConnectFour.COMPUTER, 1);
+        ConnectFour.dropPiece(board, ConnectFour.COMPUTER, 2);
+        // Computer 4 forces human to block at 3, but then computer wins at 3
+        // OR Computer 3 forces human to block at 3 or 4, computer still wins
+        countTest(4 == ConnectFour.bestMoveForComputer(board, 2) ||
+                3 == ConnectFour.bestMoveForComputer(board, 2));
+        // This sequence can't be seen in a shallow search
+        countTest(4 != ConnectFour.bestMoveForComputer(board, 1) &&
+                3 != ConnectFour.bestMoveForComputer(board, 1));
+    }
+
+    private static void clearCounts() {
+        correctTests = 0;
+        totalTests = 0;
+    }
+
+    private static void countTest(boolean correct) {
+        if(correct) {
+            correctTests++;
+        } else {
+            // Uncomment next line for a trace of which test failed.
+            //new Exception("Failed Test").printStackTrace();
+        }
+        totalTests++;
+    }
+
+    public static void main(String[] args) {
+        clearCounts();
+        testConstants();
+        testGetOpposite();
+        testDropPiece();
+        testIsLegal();
+        testIsFull();
+        testHorizontalWinner();
+        testVerticalWinner();
+        testVerticalWinner2();
+        testDiagonalWinner1();
+        testDiagonalWinner2();
+        testUndoDrop();
+        testMax0();
+        testMax1();
+        testMax2();
+        testMin3();
+        testBestMoveForComputer();
+        testDeepSearch();
+
+        System.out.println("Passed " + correctTests + " out of " + totalTests + " tests.");
+    }
+}
